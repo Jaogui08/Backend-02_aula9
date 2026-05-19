e8bafd4bc630bbc18288c6f5692917338606a961
Jaogui08 <salesfabriciojg@gmail.com>
2026-05-19 16:42:03 -0300

    Implementação Estoque

---
+++ src/main/java/com/projetologinjoaogui/entity/Estoque.java
@@ -0,0 +1,39 @@
+package com.projetologinjoaogui.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "estoque")
+public class Estoque {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@NotBlank
+	@Size(min = 2, max = 255)
+	private String localizacao;
+	
+	@NotNull
+	private int quantidade;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "id_produto")
+	private Produtos produtos;
+}
---
+++ src/main/java/com/projetologinjoaogui/repository/EstoqueRepository.java
@@ -0,0 +1,9 @@
+package com.projetologinjoaogui.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetologinjoaogui.entity.Estoque;
+
+public interface EstoqueRepository extends JpaRepository<Estoque, Long>{
+
+}
